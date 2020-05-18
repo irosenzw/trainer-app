0a4e39884d11d42a230ce0208691b6ffea33500e
Ido Rosenzwig <irosenzw@redhat.com>
2020-05-18 20:33:00 +0300

    Add reaction timer - just skeleton
    Signed-off-by: Ido Rosenzwig <irosenzw@redhat.com>

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
 import IntervalWorkout from './src/screens/Workouts/IntervalWorkout';
 import CounterWorkout from './src/screens/Workouts/CounterWorkout';
+import ReactionWorkout from './src/screens/Workouts/ReactionWorkout';
 import ActionScreen from './src/screens/ActionScreen';
 import CustomWorkout from './src/screens/Workouts/CustomWorkout';
 
@@ -17,6 +18,7 @@ export default class App extends React.PureComponent {
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Interval" component={IntervalWorkout} />
           <Stack.Screen name="Counter" component={CounterWorkout} />
+          <Stack.Screen name="Reaction" component={ReactionWorkout} />
           <Stack.Screen name="Action" component={ActionScreen} />
           <Stack.Screen name="Custom" component={CustomWorkout} />
         </Stack.Navigator>
